9710278f3a92aacaebff303a9f563755c324b254
otto281 <otto_281@hotmail.com>
2022-10-22 10:26:33 +1100

    pull from ap

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,29 @@ import DrinkCard from "./components/DrinkCard";
 
 function App() {
   const [search, setSearch] = useState("");
+  const [drinks, setDrinks] = useState([]);
 
   function checkValue(e) {
     alert(search);
+  }
+
+  function searchDrink() {
+    var requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      `https://thecocktaildb.com/api/json/v1/1/search.php?s=${search}`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        setDrinks(result.drinks);
+        console.log(result);
+        console.log(drinks);
+      })
+      .catch((error) => console.log("error", error));
   }
 
   return (
@@ -27,17 +47,14 @@ function App() {
         </Grid>
       </Grid>
       <Grid mt={5} mb={5} container spacing={3}>
-        <Grid item xs={4}>
-          <DrinkCard></DrinkCard>
-        </Grid>
-        <Grid item xs={4}>
-          <DrinkCard></DrinkCard>
-        </Grid>
-        <Grid item xs={4}>
-          <DrinkCard></DrinkCard>
-        </Grid>
+        {drinks.map((d) => (
+          <Grid item xs={4} key={d.idDrink}>
+            {" "}
+            <DrinkCard></DrinkCard>
+          </Grid>
+        ))}
       </Grid>
-      <Button onClick={checkValue} variant="contained">
+      <Button onClick={searchDrink} variant="contained">
         Check Value
       </Button>
     </div>
